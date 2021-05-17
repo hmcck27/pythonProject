086868a6463655f684c324b1d737effb8f1e98d6
backend <backend@JK.local>
2021-05-17 17:01:49 +0900

    flask server commit

--- app.py
+++ app.py
@@ -1,25 +1,12 @@
 from flask import Flask
-import model
 
 app = Flask(__name__)
 
-'''
-    
-    this is for flask service
-    
-    1. post api for get problem_set
-    
-    
-    
-'''
-
-print("hello world!")
 
 @app.route('/')
-def hello_world() :
+def hello() :
     return 'hello world!'
 
-
 if __name__ == "__main__" :
-    app.run()
+    app.run(host='0.0.0.0')
 
